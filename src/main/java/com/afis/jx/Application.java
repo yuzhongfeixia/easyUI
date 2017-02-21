a44fdbffa4a960f795b6a50336bcb9b61c6692f4
chengen <chengen@auto-fis.com>
2017-02-21 16:11:27 +0800

    初始化

---
+++ src/main/java/com/afis/jx/Application.java
@@ -0,0 +1,20 @@
+package com.afis.jx;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication(scanBasePackages = { "com.afis.jx" })
+public class Application {
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		long begin = System.currentTimeMillis();
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		long end = System.currentTimeMillis();
+		logger.info("============程序启动成功!      耗时间:{}毫秒==============", end - begin);
+	}
+}
